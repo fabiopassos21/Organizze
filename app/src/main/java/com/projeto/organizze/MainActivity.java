58c284e427e6fb2a10403e877da7ef70269f0698
fabiopassos21 <65675931+fabiopassos21@users.noreply.github.com>
2022-08-24 23:18:48 -0300

    Organizze 1.0.1 Tela cadastro

--- app/src/main/java/com/projeto/organizze/MainActivity.java
+++ app/src/main/java/com/projeto/organizze/MainActivity.java
@@ -6,15 +6,19 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 //slider
+import com.google.firebase.auth.FirebaseAuth;
 import com.heinrichreimersoftware.materialintro.app.IntroActivity;
 import com.heinrichreimersoftware.materialintro.slide.FragmentSlide;
+import com.projeto.organizze.config.ConfiguracaoFirebase;
 
 public class MainActivity extends IntroActivity {
+    private FirebaseAuth autenticacao;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //  setContentView(R.layout.activity_main);
-
+        verificarUsuarioLogado();
         setButtonBackVisible(false);
         setButtonNextVisible(false);
 
@@ -45,16 +49,34 @@ public class MainActivity extends IntroActivity {
 
 
     }
-        public void btEntrar(View view){
-            startActivity(new Intent(this, LoginActivity.class));
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        verificarUsuarioLogado();
+    }
+
+    public void btEntrar(View view) {
+        startActivity(new Intent(this, LoginActivity.class));
+    }
+
+    public void btCadastrar(View view) {
+        startActivity(new Intent(this, CadastroActivity.class));
+    }
+
+
+    public void verificarUsuarioLogado(){
+        autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
+
+        if( autenticacao.getCurrentUser() != null ){
+            abrirTelaPrincipal();
+          // autenticacao.signOut();
         }
-
-        public void btCadastrar(View view){
-            startActivity(new Intent(this, CadastroActivity.class));
-        }
+    }
 
 
-
-
+    public void abrirTelaPrincipal(){
+        startActivity(new Intent(this, PrincipalActivity.class));
+    }
 
 }
